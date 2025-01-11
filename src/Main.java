809571bc32ddc829f469c876da678daf29e99898
besartdalipi <besartdalipi2@gmail.com>
2025-01-11 15:08:33 +0100

    Added exponentiation operation to the calculator

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,14 @@ public class Main {
         System.out.println("Multiplication: " + multiply(a, b));
         System.out.println("Division: " + divide(a, b));
         System.out.println("Modulus: " + modulus(a, b));
+        System.out.println("Exponentiation: " + exponentiation(a, b));
 
     }
+
+    public static int exponentiation(int a, int b) {
+        return (int) Math.pow(a, b);
+    }
+
 
     public static int modulus(int a, int b) {
         return a % b;
